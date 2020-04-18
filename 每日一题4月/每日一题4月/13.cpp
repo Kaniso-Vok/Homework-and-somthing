bac24ae7a916be9965cfb6ce7ffa28bcfa500002
sonissa <48381669+sonissa@users.noreply.github.com>
2020-04-18 23:29:56 +0800

    update

---
+++ 每日一题4月/每日一题4月/13.cpp
@@ -0,0 +1,32 @@
+//Êý¸ù
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int judge(int& a){
+//	int aa = a;
+//	int sum = 0;
+//	while (aa){
+//		int tmp = aa % 10;
+//		sum += tmp;
+//		aa /= 10;
+//	}
+//	if (sum<10){
+//		return sum;
+//	}
+//	else{
+//		return judge(sum);
+//	}
+//}
+//
+//int main(){
+//	string str;
+//	while( cin >> str){
+//		int sum=0;
+//		for (auto a : str){
+//			sum += a - '0';
+//		}
+//		cout << judge(sum) << endl;
+//	}
+//	return 0;
+//}
---
+++ 每日一题4月/每日一题4月/14.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//快到碗里来
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	double a, b;
+//	while (cin >> a >> b){
+//		if (b*3.14 * 2 >= a){
+//			cout << "Yes" << endl;
+//		}
+//		else{
+//			cout << "No" << endl;
+//		}
+//	}
+//	return 0;
+//}
