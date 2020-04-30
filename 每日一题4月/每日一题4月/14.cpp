f4dea54bca41707d5c3c35d45e39a5fe207a1dcc
sonissa <48381669+sonissa@users.noreply.github.com>
2020-04-30 23:51:58 +0800

    update

--- 每日一题4月/每日一题4月/14.cpp
+++ 每日一题4月/每日一题4月/14.cpp
@@ -1,25 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //斐波那契凤尾
 #include <iostream>
-#include <vector>
-using namespace std;
-int main(){
-	vector<int> v;
-	v.resize(100001);
-	v[0] = v[1] = 1;
-	for (int i = 2; i<100001; i++){
-		v[i] = (v[i - 1] + v[i - 2]) % 1000000;
-	}
-	int n;
-	while (cin>>n){
-		if (n >= 29){
-			printf("%06d\n", v[n]);
-		}
-		else
-			cout << v[n] << endl;
-	}
-	return 0;
-}
+//#include <vector>
+//using namespace std;
+//int main(){
+//	vector<int> v;
+//	v.resize(100001);
+//	v[0] = v[1] = 1;
+//	for (int i = 2; i<100001; i++){
+//		v[i] = (v[i - 1] + v[i - 2]) % 1000000;
+//	}
+//	int n;
+//	while (cin>>n){
+//		if (n >= 29){
+//			printf("%06d\n", v[n]);
+//		}
+//		else
+//			cout << v[n] << endl;
+//	}
+//	return 0;
+//}
 
 
 //快到碗里来
